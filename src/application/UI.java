ec119004d2dea2b8ffa53542c059113d6b846fdb
Julia Mayumi <juliamaytm@gmail.com>
2020-12-23 11:34:50 -0300

    Chess layer and printing the board

---
+++ src/application/UI.java
@@ -0,0 +1,26 @@
+package application;
+
+import chess.ChessPiece;
+
+public class UI {
+
+	public static void printBoard(ChessPiece[][] chessPieces) {
+		for (int i = 0; i<chessPieces.length; i++) {
+			System.out.print((8 - i) + " ");
+			for(int j = 0; j<chessPieces.length; j++) {
+				printPiece(chessPieces[i][j]);
+			}
+			System.out.println();
+		}
+		System.out.println("  a b c d e f g h");
+	}
+
+	public static void printPiece(ChessPiece chessPieces) {
+		if (chessPieces == null) {
+			System.out.print("-");
+		} else {
+			System.out.print(chessPieces);
+		}
+		System.out.print(" ");
+	}
+}
